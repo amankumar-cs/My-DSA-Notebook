e79f65931ee5a20dc4a2b7126572c5c67c766429
Macbook Air M1 <macbookairm1@Macbooks-MacBook-Air-2.local>
2025-06-28 21:08:26 +0530

    Added new code

--- Hashing/frequencyOfElementsInRange.cpp
+++ Hashing/frequencyOfElementsInRange.cpp
@@ -19,7 +19,7 @@ int main(){
     cout<<"Enter the number of elements: ";
     cin>>n;
     vector<int> arr(n);
-    cout<<"Enter the elements: ";
+    cout<<"Enter the elements in the range of 1 to "<<n<<": ";
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }

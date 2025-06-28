a8530dca925b927b43164ecfe9c2976220218c38
Macbook Air M1 <macbookairm1@Macbooks-MacBook-Air-2.local>
2025-06-28 17:11:21 +0530

    Added new code

--- Maths/checkPalindrome.cpp
+++ Maths/checkPalindrome.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 
 bool isPalindrome(int x) {
-        int rev = 0;
-        if(x<0 || (x % 10 == 0 && x!=0)){
-            return false;
+    int rev = 0;
+    if(x<0 || (x % 10 == 0 && x!=0)){
+        return false;
+    }
+    int temp = x;
+    while(x!=0){
+        int pop = x % 10;
+        if(rev>INT_MAX/10 || (rev==INT_MAX/10 && pop>7)){
+            return 0;
         }
-        int temp = x;
-        while(x!=0){
-            int pop = x % 10;
-            if(rev>INT_MAX/10 || (rev==INT_MAX/10 && pop>7)){
-                return 0;
-            }
-            rev = rev*10 + pop;
-            x = x/10;
-        }
-        if(rev==temp){
-            return true;
-        }
-        else{
-            return false;
-        }
+        rev = rev*10 + pop;
+        x = x/10;
+    }
+    if(rev==temp){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 int main(){

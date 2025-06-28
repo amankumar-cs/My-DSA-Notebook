25f9d7e97f9b7aad5bba490bc7966738dfa9cb97
Macbook Air M1 <macbookairm1@Macbooks-MacBook-Air-2.local>
2025-06-28 20:53:50 +0530

    Added new code

---
+++ Hashing/frequencyOfElementsInRange.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+vector<int> frequencyCount(vector<int>& arr) {
+    // code here
+    vector<int> nums(arr.size());
+    map<int,int> mpp;
+    for(int i=0;i<arr.size();i++){
+        mpp[arr[i]]++;
+    }
+    for(auto it: mpp){
+        nums[it.first-1] = it.second;
+    }
+    return nums;
+}
+int main(){
+    int n;
+    cout<<"Enter the number of elements: ";
+    cin>>n;
+    vector<int> arr(n);
+    cout<<"Enter the elements: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    vector<int> nums = frequencyCount(arr);
+    cout<<"The frequency count of elements from 1 to "<<n<<" is given as: [";
+    for(int i=0;i<n;i++){
+        if(i==n-1){
+            cout<<nums[i];
+            break;
+        }
+        cout<<nums[i]<<", ";
+    }
+    cout<<"]"<<endl;
+
+    return 0;
+}

c3d1c21466efb9786bc8cf302d7581ce4660acb8
Macbook Air M1 <macbookairm1@Macbooks-MacBook-Air-2.local>
2025-08-13 23:02:16 +0530

    Added new code

---
+++ Arrays/two_sum_using_hashing.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<map>
+using namespace std;
+int main(){
+    vector<int> arr = {2,6,5,8,11};
+    int target = 10;
+    int n = arr.size();
+    int flag = 0;
+    map<int,int> mpp;
+    for(int i=0;i<n;i++){
+        int curr = arr[i];
+        int rem = target-curr;
+        if(mpp.find(rem)!=mpp.end()){
+            cout<<"The required indices are: ("<<mpp[rem]<<","<<i<<")"<<endl;
+            flag = 1;
+            break;
+        }
+        mpp[curr] = i;
+    }
+    cout<<"No sum exists for the given target"<<endl;
+    return 0;
+}
